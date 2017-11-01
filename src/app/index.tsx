9384d01787269e99a233c98b207b46a85e663f46
horup <sah@sah.dk>
2017-11-01 11:23:05 +0100

    cleaned up

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import { DatetimePicker } from '../extensions';
 import { HashRouter, Route, Link } from 'react-router-dom';
 import { HomeView, FormsView, ChartsView } from './views';
 import Header from './header';
